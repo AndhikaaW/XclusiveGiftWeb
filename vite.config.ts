f1ac928593e3397d8be7cb27a5bf437120fd7521
AndhikaW <andhikaw61@gmail.com>
2026-05-15 17:44:13 +0700

    feat : vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // Change this to match your GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/XclusiveGiftWeb/',
+  base: '/',
 })
